981f4089a550e10893fab72d7675ab1a97ccef5a
goofy <oemer.yildiz@tuhh.de>
2020-09-21 18:57:21 +0200

    fixed false escapes

--- config.def.h
+++ config.def.h
@@ -63,8 +63,8 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-  { battery_perc, "Battery: %s\% | ", "BAT0" },
-  { cpu_perc, "CPU: %s\% | ", NULL },
-  { ram_perc, "RAM: %s\% | ", NULL },
+  { battery_perc, "Battery: %s % | ", "BAT0" },
+  { cpu_perc, "CPU: %s % | ", NULL },
+  { ram_perc, "RAM: %s % | ", NULL },
   { datetime, "%s", "%a %F %T" },
 };
